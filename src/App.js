07069f1735dc5ce24ccb4b03792d2d939b1160b9
MSI\sudip <sudip.embedded@gmail.com>
2024-08-08 21:21:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import  "./index.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="BodyWrapper">
+      <h1 > This is Test  Header</h1>
+      <input></input>
+      <br></br>
+  
+      <button className="ButtonWrapper">Submit</button>
+    
+    
+
+      <h3 > This is Footer  </h3>
+     </div>
   );
 }
 
